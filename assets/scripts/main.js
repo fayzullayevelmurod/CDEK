87daee2b92d8e1b77ac8d42b210f53e05372c477
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2026-05-14 01:04:47 +0500

    contact and footer the end

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,115 @@
+// select-custom
+const select = document.getElementById("customSelect");
+const head = select.querySelector(".select-head");
+const options = select.querySelectorAll(".option");
+const text = head.querySelector("span");
 
-let swiper = new Swiper(".treatmentsSwiper", {
-  slidesPerView: 1.10,
-  spaceBetween: 28,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2.2,
-      spaceBetween: 28,
-    },
-    993: {
-      slidesPerView: 3.2,
-      spaceBetween: 28,
-    },
-    1200: {
-      slidesPerView: 3.6,
-      spaceBetween: 40,
-    },
-  },
+head.addEventListener("click", () => {
+  select.classList.toggle("open");
 });
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    text.textContent = option.textContent;
+    select.classList.remove("open");
+
+    select.classList.remove("error");
+  });
+});
+
+document.addEventListener("click", (e) => {
+  if (!select.contains(e.target)) {
+    select.classList.remove("open");
+  }
+});
+// select-custom
+
+// tab
+const tabs = document.querySelector(".tabs-container");
+const buttons = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".content");
+
+tabs.addEventListener("click", (e) => {
+  const button = e.target.closest(".tab-btn");
+
+  if (!button) return;
+
+  const id = button.dataset.id;
+
+  buttons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  button.classList.add("active");
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  const element = document.getElementById(id);
+
+  if (element) {
+    element.classList.add("active");
+  }
+});
+// tab
+
+// contact
+const form = document.querySelector(".contact_in form");
+const contactIn = document.querySelector(".contact_in");
+const contactEd = document.querySelector(".contact_end");
+
+const inputs = form.querySelectorAll("input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let isValid = true;
+  inputs.forEach((input) => {
+    if (input.value.trim() === "") {
+      isValid = false;
+      input.classList.add("error");
+    } else {
+      input.classList.remove("error");
+    }
+  });
+
+  if (text.textContent.trim() === "Маркетплейс") {
+    isValid = false;
+    select.classList.add("error");
+  } else {
+    select.classList.remove("error");
+  }
+
+  // Success
+  if (isValid) {
+    contactIn.classList.add("active");
+    contactEd.classList.add("active");
+  }
+});
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.value.trim() !== "") {
+      input.classList.remove("error");
+    }
+  });
+});
+
+const prevBtn = document.querySelector(".prev_btn");
+
+prevBtn.addEventListener("click", () => {
+  form.reset();
+
+  text.textContent = "Маркетплейс";
+
+  contactIn.classList.remove("active");
+  contactEd.classList.remove("active");
+
+  inputs.forEach((input) => {
+    input.classList.remove("error");
+  });
+
+  select.classList.remove("error");
+});
+// contact
